582284ba2e502c2010969b0a23282fb878aed5a4
ega-erinovian <erinovian69@gmail.com>
2024-11-18 10:07:00 +0700

    fixed missing metadata

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,4 +1,11 @@
 import ServicesPage from "@/features/services";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Archi Teknik | Services",
+  description:
+    "Discover exceptional architectural and engineering consulting services, offering innovative designs, precise planning, and sustainable solutions for projects worldwide. Partner with experts to bring your vision to life.",
+};
 
 const ServiceDetail = () => {
   return <ServicesPage />;
